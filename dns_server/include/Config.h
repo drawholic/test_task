cc1b8b3bdb06f9e1c503bc350943fa4e697810f7
drawholic <mamrokhavlad@gmail.com>
2025-07-06 11:00:10 +0300

    added includes. response type enum. Config struct updated

--- dns_server/include/Config.h
+++ dns_server/include/Config.h
@@ -1,23 +1,44 @@
-#include <arpa/inet.h>
+// #include <arpa/inet.h>
 
-// #define INET_ADDRSTRLEN 20 
-#define BUFLEN 256
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+// // #define INET_ADDRSTRLEN 20 
+// #define BUFLEN 256
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <fcntl.h>
 
-struct Config
-{
-	char upstream_ip[INET_ADDRSTRLEN];
-	int upstream_port;
-	char **blacklist;
-	int blacklist_count;
-	enum ResponseType{
-		NXDOMAIN, REFUSED, CUSTOM_IP
-	} response_type;
-	char custom_ip[INET_ADDRSTRLEN];
+// struct Config
+// {
+// 	char upstream_ip[INET_ADDRSTRLEN];
+// 	int upstream_port;
+// 	char **blacklist;
+// 	int blacklist_count;
+// 	enum ResponseType{
+// 		NXDOMAIN, REFUSED, CUSTOM_IP
+// 	} response_type;
+// 	char custom_ip[INET_ADDRSTRLEN];
+// };
+
+// void read_config(struct Config c);
+
+#pragma once
+
+#include <netinet/in.h>
+
+#define MAX_BLACKLIST 1024
+#define MAX_LINE 256
+
+enum ResponseType {
+	RESP_NXDOMAIN,
+	RESP_REFUSED,
+	RESP_CUSTOM_IP
 };
 
-void read_config(struct Config c);
+struct Config {
+	char upstream_ip[INET_ADDRSTRLEN];
+	enum ResponseType response_type;
+	char *blacklist[MAX_BLACKLIST];
+	int blacklist_count;
+};
 
+void read_config(struct Config* c);
